614b06fc65054be247f36c99d18fe0a093a7c5d7
Zeeshan Ahmad <itexpert120@outlook.com>
2025-01-10 00:28:09 +0500

    revert to 5000

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from waitress import serve
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 56417))
+    port = int(os.environ.get("PORT", 5000))
     serve(app, host="127.0.0.1", port=port)
